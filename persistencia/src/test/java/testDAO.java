51ef574f4f4736431fb22f2679ee470ac36c1956
Yael_ <cristhian.castanos@uabc.edu.mx>
2025-09-15 14:05:09 -0700

    estado base que compila

---
+++ persistencia/src/test/java/testDAO.java
@@ -0,0 +1,17 @@
+import mx.avanti.desarrollo.dao.AlumnoDAO;
+import mx.avanti.desarrollo.persistence.HibernateUtil;
+import mx.desarrollo.entity.Alumno;
+
+import java.util.List;
+
+public class testDAO {
+
+    public static void main(String[] args) {
+        AlumnoDAO alumnoDAO = new AlumnoDAO(HibernateUtil.getEntityManager());
+
+
+        for (Alumno alumno : alumnoDAO.findAll()) {
+            System.out.println(alumno + "|| id [" + alumno.getId()+ "]");
+        }
+    }
+}
